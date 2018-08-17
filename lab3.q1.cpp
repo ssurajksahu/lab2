b81191e17612fe34a81278e1faab47f4eaf04d9e
ssurajksahu <42214254+ssurajksahu@users.noreply.github.com>
2018-08-17 21:40:11 +0530

    Add files via upload

---
+++ lab3.q1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+char a='a';
+int b=4;
+float c=4.98;
+double d=6.3443;
+bool e=4378;
+
+cout <<"size of char:"<<sizeof(char)<<"byte"<<end1;
+cout <<"size of int:"<<sizeof(int)<<"byte"<<end1;
+cout <<"size of float:"<<sizeof(float)<<"byte"<<end1;
+cout <<"size of double:"<<sizeof(double)<<"byte"<<end1;
+cout <<"size of bool:"<<sizeof(bool)<<"byte"<<end1;
+cout <<"size of doubleVar:"<<sizeof(doubleVar)<<"byte"<<end1:
+
+return 0;
+}
+
---
+++ lab3.q2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a=3;
+int b=5;
+int c;
+
+
+c= a+b;
+cout <<"1.value of c is:"<<c<<end1;
+c=a-b;
+cout <<"2.value of c is:"<<c<<end1;
+c=a*b;
+cout  <<"3.value of c is:"<<c<<end1;
+c=a/b;
+cout  <<"4.value of c is:"<<c<<end1;
+c=a%b;
+cout <<"5.value of c is:"<<c<<end1;
+return 0;
+{
